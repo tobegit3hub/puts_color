db8b762cb9d6e7a27348342aaf034d0df3efc1b6
tobe <tobegit3hub@gmail.com>
2013-12-15 17:34:42 +0800

    add module for all puts function

--- lib/puts_color.rb
+++ lib/puts_color.rb
@@ -1,43 +1,47 @@
 #!/usr/bin/env ruby
 
-def puts_red(*strings)
-  strings.each do |string|
-    puts "\033[0;31m" + string + "\033[0m"
-  end
-end
+module PutsColor
 
-def puts_green(*strings)
-  strings.each do |string|
-    puts "\033[0;32m" + string + "\033[0m"
+  def puts_red(*strings)
+    strings.each do |string|
+      puts "\033[0;31m" + string + "\033[0m"
+    end
   end
-end
 
-def puts_yellow(*strings)
-  strings.each do |string|
-    puts "\033[0;33m" + string + "\033[0m"
+  def puts_green(*strings)
+    strings.each do |string|
+      puts "\033[0;32m" + string + "\033[0m"
+    end
   end
-end
 
-def puts_blue(*strings)
-  strings.each do |string|
-    puts "\033[0;34m" + string + "\033[0m"
+  def puts_yellow(*strings)
+    strings.each do |string|
+      puts "\033[0;33m" + string + "\033[0m"
+    end
   end
-end
 
-def puts_purple(*strings)
-  strings.each do |string|
-    puts "\033[0;35m" + string + "\033[0m"
+  def puts_blue(*strings)
+    strings.each do |string|
+      puts "\033[0;34m" + string + "\033[0m"
+    end
   end
-end
 
-def puts_cyan(*strings)
-  strings.each do |string|
-    puts "\033[0;36m" + string + "\033[0m"
+  def puts_purple(*strings)
+    strings.each do |string|
+      puts "\033[0;35m" + string + "\033[0m"
+    end
   end
-end
 
-def puts_grey(*strings)
-  strings.each do |string|
-    puts "\033[0;37m" + string + "\033[0m"
+  def puts_cyan(*strings)
+    strings.each do |string|
+      puts "\033[0;36m" + string + "\033[0m"
+    end
   end
+
+  def puts_grey(*strings)
+    strings.each do |string|
+      puts "\033[0;37m" + string + "\033[0m"
+    end
+  end
+
 end
